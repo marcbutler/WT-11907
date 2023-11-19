e25dd6e8d6a475ea67566515f91d105ad1f7f3c2
Marc Butler <135883+marcbutler@users.noreply.github.com>
2023-11-19 22:23:11 +0000

    Add simple test of a singular thread
    Does not flag a data race as expected

--- tiny-datarace.cc
+++ tiny-datarace.cc
@@ -6,10 +6,11 @@ namespace {
     const int kThreadCount = 100;
     const int kOpCount = 10000;
 
-    int global = 0;
+    volatile int global = 0;
+    volatile int global2 = 0;
 }
 
-void atomic_write(int *value) {
+void atomic_write(volatile int *value) {
     for (int i = 0; i < kOpCount; i++) {
         __atomic_store_n(value, i, __ATOMIC_SEQ_CST);
     }
@@ -17,7 +18,7 @@ void atomic_write(int *value) {
 
 #ifdef ATOMIC_ONLY
 
-void read_value(int *value) {
+void read_value(volatile int *value) {
     for (int i = 0; i < kOpCount; i++) {
         int v = __atomic_load_n(value, __ATOMIC_SEQ_CST);
         assert(v >= 0 && v <= kOpCount);
@@ -26,7 +27,7 @@ void read_value(int *value) {
 
 #else
 
-void read_value(int *value) {
+void read_value(volatile int *value) {
     for (int i = 0; i < kOpCount; i++) {
         int v = *value;
         assert(v >= 0 && v <= kOpCount);
@@ -35,8 +36,11 @@ void read_value(int *value) {
 
 #endif
 
-int main(int argc [[maybe_unused]], char **argv [[maybe_unused]])
+
+void test_multiple_threads()
 {
+    // Mutliple threads writing and reading the same memory location.
+
     std::thread thrs[kThreadCount];
 
     for (auto i = 0; i < kThreadCount; i++) {
@@ -49,5 +53,52 @@ int main(int argc [[maybe_unused]], char **argv [[maybe_unused]])
     for (auto i = 0; i < kThreadCount; i++) {
         thrs[i].join();
     }
+
+}
+
+
+void singular_access(volatile int *value) {
+    int v;
+
+    __atomic_store_n(value, -1, __ATOMIC_SEQ_CST);
+#if ATOMIC_ONLY
+    v = __atomic_load_n(value, __ATOMIC_SEQ_CST);
+    assert(v == -1);
+#else
+    v = global;
+    assert(v == -1);
+#endif
+    __atomic_store_n(value, 0, __ATOMIC_SEQ_CST);
+}
+
+
+void test_single_thread_only()
+{
+    // If only a single thread issues a write then read against a single location is a data race
+    // still detected: it shouldn't be as there is no possibility of simultaneous access.
+
+    std::thread thrs[kThreadCount];
+
+    for (int i = 0; i < kThreadCount; i++) {
+        if (i != kThreadCount / 2)
+            thrs[i] = std::thread(read_value, &global);
+        else
+            thrs[i] = std::thread(read_value, &global2);
+    }
+
+    for (auto&& t : thrs) {
+        t.join();
+    }
+}
+
+
+int main(int argc [[maybe_unused]], char **argv [[maybe_unused]])
+{
+    std::cout << "\nACCESS FROM MULTIPLE THREADS\n" << std::endl;
+    test_multiple_threads();
+
+    std::cout << "\nACCESS FROM A SINGLE THREAD ONLY\n" << std::endl;
+    test_single_thread_only();
+
     return 0;
 }
